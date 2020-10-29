804b3a2ad05155288afe37a339615e7be67b4973
vfedushchin <vfedushchin@fieldengineer.com>
2020-10-29 18:28:15 +0300

    comment fio connected menu items from settings screen

--- app/modules/Settings/SettingsMainScreen.js
+++ app/modules/Settings/SettingsMainScreen.js
@@ -424,7 +424,7 @@ class SettingsMainScreen extends Component {
                                     </View>
                                 </TouchableOpacity>
 
-                                <TouchableOpacity style={{ ...styles.block__item }} onPress={() => NavStore.goNext('FioSendRequest')}>
+                                {/*<TouchableOpacity style={{ ...styles.block__item }} onPress={() => NavStore.goNext('FioSendRequest')}>
                                     <Icon name="exchangeRates" size={20} style={styles.icon}/>
                                     <View style={styles.block__item__content}>
                                         <Text style={styles.block__text}>{strings('settings.walletManagement.sendFioRequest')}</Text>
@@ -463,7 +463,7 @@ class SettingsMainScreen extends Component {
                                     <View style={styles.block__item__arrow}>
                                         <Ionicons name="ios-arrow-forward" size={20} style={styles.block__arrow}/>
                                     </View>
-                                </TouchableOpacity>
+                                </TouchableOpacity>*/}
                             </View>
                         </View>
 
