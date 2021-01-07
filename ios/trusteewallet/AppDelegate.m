5f5a867d915245b60e97ce7c2243534e940113fe
ksu <ksu.zhytomirsky@gmail.com>
2021-01-07 21:03:04 +0200

    fx

--- ios/trusteewallet/AppDelegate.m
+++ ios/trusteewallet/AppDelegate.m
@@ -15,7 +15,6 @@
 #import <React/RCTRootView.h>
 #import <React/RCTLinkingManager.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <TSBackgroundFetch/TSBackgroundFetch.h>
 #import "Orientation.h"
 
 @implementation AppDelegate
@@ -30,30 +29,30 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
-  
+
   [[FBSDKApplicationDelegate sharedInstance] application:application
   didFinishLaunchingWithOptions:launchOptions];
-  
+
   [FIROptions defaultOptions].deepLinkURLScheme = @"com.trusteewallet";
   [FIRApp configure];
   [[TSBackgroundFetch sharedInstance] didFinishLaunching];
-  
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"TrusteeWallet"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [UIColor backgroundColor];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
+
   [FBSDKSettings setAutoLogAppEventsEnabled:YES];
   [FBSDKSettings setAdvertiserIDCollectionEnabled:YES];
-  
+
   return YES;
 }
 
