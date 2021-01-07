78c6252a4be21ad03261dd92756069b0373b74a8
ksu <ksu.zhytomirsky@gmail.com>
2021-01-07 15:36:03 +0200

    ios version up and cleaned

--- ios/trusteewallet/AppDelegate.m
+++ ios/trusteewallet/AppDelegate.m
@@ -7,9 +7,8 @@
 
 #import "AppDelegate.h"
 #import <Firebase.h>
-#import "RNFirebaseNotifications.h"
-#import "RNFirebaseMessaging.h"
-#import "RNFirebaseLinks.h"
+#import "FirebaseMessaging.h"
+#import "FirebaseDynamicLinks.h"
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -37,7 +36,6 @@
   
   [FIROptions defaultOptions].deepLinkURLScheme = @"com.trusteewallet";
   [FIRApp configure];
-  [RNFirebaseNotifications configure];
   [[TSBackgroundFetch sharedInstance] didFinishLaunching];
   
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
@@ -59,43 +57,6 @@
   return YES;
 }
 
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-            options:(NSDictionary<NSString *, id> *)options {
-    BOOL handledLink = [[RNFirebaseLinks instance] application:application openURL:url options:options];
-
-    if (!handledLink) {
-        handledLink = [RCTLinkingManager application:application openURL:url options:options];
-    }
-  
-    if ([[FBSDKApplicationDelegate sharedInstance] application:application openURL:url options:options]) {
-      return YES;
-    }
-  
-    return handledLink;
-}
-
-- (BOOL)application:(UIApplication *)application
-continueUserActivity:(NSUserActivity *)userActivity
- restorationHandler:(void (^)(NSArray *))restorationHandler {
-     BOOL handleRestore = [[RNFirebaseLinks instance] application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-
-     if (handleRestore) {
-       handleRestore = [RCTLinkingManager application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
-     }
-     return handleRestore;
-}
-
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-  [[RNFirebaseNotifications instance] didReceiveLocalNotification:notification];
-}
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo
-fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler{
-  [[RNFirebaseNotifications instance] didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
-- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
-  [[RNFirebaseMessaging instance] didRegisterUserNotificationSettings:notificationSettings];
-}
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
