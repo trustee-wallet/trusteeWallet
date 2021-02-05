86a90edd8daf61eb08876c45b2c3a41d43b8f189
ksu <ksu.zhytomirsky@gmail.com>
2021-02-05 20:31:16 +0200

    stellar hd wallet is actually not needed

--- crypto/blockchains/xlm/XlmAddressProcessor.js
+++ crypto/blockchains/xlm/XlmAddressProcessor.js
@@ -2,7 +2,39 @@
  * @version 0.20
  * https://github.com/chatch/stellar-hd-wallet/blob/master/src/stellar-hd-wallet.js
  */
-import StellarHDWallet from 'stellar-hd-wallet'
+import BlocksoftKeys from '../../actions/BlocksoftKeys/BlocksoftKeys'
+
+const createHmac = require('create-hmac')
+const ED25519_CURVE = 'ed25519 seed'
+const HARDENED_OFFSET = 0x80000000
+
+const StellarSdk = require('stellar-sdk')
+
+function getMasterKeyFromSeed(seed) {
+    const hmac = createHmac('sha512', ED25519_CURVE)
+    const I = hmac.update(Buffer.from(seed, 'hex')).digest()
+    const IL = I.slice(0, 32)
+    const IR = I.slice(32)
+    return {
+        key: IL,
+        chainCode: IR
+    }
+}
+
+function CKDPriv(_ref, index) {
+    const key = _ref.key
+    const chainCode = _ref.chainCode
+    const indexBuffer = Buffer.allocUnsafe(4)
+    indexBuffer.writeUInt32BE(index, 0)
+    const data = Buffer.concat([Buffer.alloc(1, 0), key, indexBuffer])
+    const I = createHmac('sha512', chainCode).update(data).digest()
+    const IL = I.slice(0, 32)
+    const IR = I.slice(32)
+    return {
+        key: IL,
+        chainCode: IR
+    }
+}
 
 export default class XlmAddressProcessor {
 
@@ -16,7 +48,17 @@ export default class XlmAddressProcessor {
      * @returns {Promise<{privateKey: string, address: string}>}
      */
     async getAddress(privateKey, data = {}, superPrivateData = {}) {
-        const wallet = StellarHDWallet.fromMnemonic(superPrivateData.mnemonic)
-        return { address: wallet.getPublicKey(0), privateKey: wallet.getSecret(0) }
+        const seed = BlocksoftKeys.getSeedCached(superPrivateData.mnemonic).toString('hex')
+        const getMaster = getMasterKeyFromSeed(seed)
+        const key = getMaster.key
+        const chainCode = getMaster.chainCode
+        const segments = [44, 148, 0]
+
+        const res = segments.reduce(function(parentKeys, segment) {
+            return CKDPriv(parentKeys, segment + HARDENED_OFFSET)
+        }, { key: key, chainCode: chainCode })
+
+        const keypair = StellarSdk.Keypair.fromRawEd25519Seed(res.key)
+        return { address: keypair.publicKey(), privateKey: keypair.secret() }
     }
 }
