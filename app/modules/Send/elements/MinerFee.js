3439426b9416057d511c9205f7b0853f4985a033
ksu <ksu.zhytomirsky@gmail.com>
2021-03-19 23:38:02 +0200

    btc change is ok #1713

--- app/modules/Send/elements/MinerFee.js
+++ app/modules/Send/elements/MinerFee.js
@@ -10,6 +10,7 @@ import Log from '@app/services/Log/Log'
 import CheckData from '@app/modules/Send/elements/CheckData'
 import { feesTitles } from '@app/modules/Send/advanced/helpers'
 import MarketingEvent from '@app/services/Marketing/MarketingEvent'
+import BlocksoftPrettyStrings from '@crypto/common/BlocksoftPrettyStrings'
 
 class MinerFee extends React.PureComponent {
 
@@ -29,6 +30,16 @@ class MinerFee extends React.PureComponent {
         } else if (typeof selectedFee.showNonce !== 'undefined' && selectedFee.showNonce) {
             nonceForTxTitle = 'send.receiptScreen.nonce'
         }
+        const isOutputs = devMode && selectedFee && typeof selectedFee.blockchainData !== 'undefined' && selectedFee.blockchainData && typeof selectedFee.blockchainData.preparedInputsOutputs !== 'undefined'
+        let changeAddress = false
+        if (isOutputs) {
+            for (const output of selectedFee.blockchainData.preparedInputsOutputs.outputs) {
+                console.log(output)
+                if (typeof output.isChange !== 'undefined' && output.isChange) {
+                    changeAddress = output.to
+                }
+            }
+        }
         return (
             <>
                 <CheckData
@@ -37,11 +48,12 @@ class MinerFee extends React.PureComponent {
                     subvalue={fiatFee}
                 />
                 {
-                    devMode && selectedFee && typeof selectedFee.blockchainData !== 'undefined' && selectedFee.blockchainData && typeof selectedFee.blockchainData.preparedInputsOutputs !== 'undefined' ?
+                    isOutputs ?
                         <CheckData
                             name={'DEV Mode'}
                             value={ selectedFee.feeForByte + ' sat/B ' + (typeof selectedFee.needSpeed !== 'undefined' && selectedFee.needSpeed ? (' rec. ' + selectedFee.needSpeed + ' sat/B') : '')}
-                            subvalue={'ins: ' + selectedFee.blockchainData.preparedInputsOutputs.inputs.length + ' outs: ' + selectedFee.blockchainData.preparedInputsOutputs.outputs.length}
+                            subvalue={'ins: ' + selectedFee.blockchainData.preparedInputsOutputs.inputs.length + ' outs: ' + selectedFee.blockchainData.preparedInputsOutputs.outputs.length
+                                        + (changeAddress ? (' ch: ' + BlocksoftPrettyStrings.makeCut(changeAddress, 4)) : '') }
                         /> : null
                 }
                 {
