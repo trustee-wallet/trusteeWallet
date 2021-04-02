8e5b920842edb4efc57d23de0f48b439ae722e24
ksu <ksu.zhytomirsky@gmail.com>
2021-04-02 21:34:01 +0200

    https://hq1.appsflyer.com/marketplace/integrated-partners/id1462924276 for #1706

--- App.js
+++ App.js
@@ -18,6 +18,25 @@ import { ThemeProvider } from '@app/modules/theme/ThemeProvider'
 import Application from '@app/appstores/Actions/App/App'
 import { SendDeepLinking } from '@app/appstores/Stores/Send/SendDeepLinking'
 
+import appsFlyer from 'react-native-appsflyer'
+
+appsFlyer.initSdk(
+    {
+        devKey: 'qUa6hSzjW3RDsTgEYQh4oT',
+        isDebug: false,
+        appId: '1462924276',
+        onInstallConversionDataListener: true, // Optional
+        onDeepLinkListener: true, // Optional
+        timeToWaitForATTUserAuthorization: 10 // for iOS 14.5
+    },
+    (result) => {
+        console.log('appsFlyer', result)
+    },
+    (error) => {
+        console.error('appsFlyer', error)
+    }
+);
+
 export default class App extends Component {
 
     // eslint-disable-next-line camelcase
