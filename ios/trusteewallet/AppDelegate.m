63b610db94a51c6d25f118d09dd677c9deb7d1ff
ksu <ksu.zhytomirsky@gmail.com>
2021-05-06 08:33:48 +0300

    dark mode in ios preload #1873 [flightalpha]

--- ios/trusteewallet/AppDelegate.m
+++ ios/trusteewallet/AppDelegate.m
@@ -37,8 +37,11 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"TrusteeWallet"
                                             initialProperties:nil];
-
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  if (@available(iOS 13.0, *)) {
+     rootView.backgroundColor = UIColor.systemBackgroundColor;
+  } else {
+     rootView.backgroundColor = UIColor.grayColor;
+  }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
