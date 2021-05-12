31a60154ee147c89d8e3cf3b734286d8b021aaac
ksu <ksu.zhytomirsky@gmail.com>
2021-05-12 16:05:29 +0300

    new fio server #1895

--- app/config/config.js
+++ app/config/config.js
@@ -88,9 +88,9 @@ const config = {
     fio: {
         apiEndpoints: {
             // baseURL: 'https://testnet.fioprotocol.io/v1/',
-            baseURL: 'https://api.fio.eosdetroit.io/v1/',
+            baseURL: 'https://fio.eosphere.io/v1/',
             // historyURL: 'https://testnet.fio.dev/v1/history/',
-            historyURL: 'https://api.fio.eosdetroit.io/v1/history/',
+            historyURL: 'https://fio.eosphere.io/v1/history/',
             registrationSiteURL: 'https://reg.fioprotocol.io/ref/trustee?publicKey=',
         },
     },
