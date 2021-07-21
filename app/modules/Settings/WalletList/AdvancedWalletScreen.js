8da699a59fdfb9366c7a18d0657ece5696cb397e
ShchipskiiYura <Shchipskii@gmail.com>
2021-07-21 18:12:01 +0300

    fx #329

--- app/modules/Settings/WalletList/AdvancedWalletScreen.js
+++ app/modules/Settings/WalletList/AdvancedWalletScreen.js
@@ -7,9 +7,9 @@ import {
     Text,
     ScrollView,
     StyleSheet,
+    TouchableOpacity,
 } from 'react-native'
 import { connect } from 'react-redux'
-import { SwipeRow } from 'react-native-swipe-list-view'
 
 import NavStore from '@app/components/navigation/NavStore'
 
@@ -20,20 +20,15 @@ import walletActions from '@app/appstores/Stores/Wallet/WalletActions'
 
 import { ThemeContext } from '@app/theme/ThemeProvider'
 import TextInput from '@app/components/elements/new/TextInput'
-import RoundButton from '@app/components/elements/new/buttons/RoundButton'
 import ListItem from '@app/components/elements/new/list/ListItem/Setting'
 import CustomIcon from '@app/components/elements/CustomIcon'
 
-import { LockScreenFlowTypes, setLockScreenConfig } from '@app/appstores/Stores/LockScreen/LockScreenActions'
-import { setBseLink, setLoaderStatus } from '@app/appstores/Stores/Main/MainStoreActions'
 import MarketingAnalytics from '@app/services/Marketing/MarketingAnalytics'
 import ScreenWrapper from '@app/components/elements/ScreenWrapper'
 import { getSelectedWalletData } from '@app/appstores/Stores/Main/selectors'
 import { getSettingsScreenData } from '@app/appstores/Stores/Settings/selectors'
 import { showModal } from '@app/appstores/Stores/Modal/ModalActions'
-import { getWalletsGeneralData } from '@app/appstores/Stores/Wallet/selectors'
-import UpdateAccountListDaemon from '@app/daemons/view/UpdateAccountListDaemon'
-import Log from '@app/services/Log/Log'
+import { getWalletsGeneralData, getWalletsNumber } from '@app/appstores/Stores/Wallet/selectors'
 
 
 class AdvancedWalletScreen extends PureComponent {
@@ -76,40 +71,18 @@ class AdvancedWalletScreen extends PureComponent {
             type: 'YES_NO_MODAL',
             icon: 'WARNING',
             title: strings('modal.titles.attention'),
-            description: strings('modal.walletDelete.deleteWallet'),
+            description: strings('modal.walletDelete.deleteWallet', { walletName: this.state.walletName }),
         },  () => {
-            this._actualDelete(true)
+            this._actualDelete()
         })
     }
 
-    _actualDelete = async (needPassword = true) => {
+    _actualDelete = () => {
 
-        const { walletHash, walletNumber, walletIsBackedUp } = this.props.selectedWalletData
-        const { totalBalance } = this.props.walletsGeneralData
-        const { lockScreenStatus } = this.props.settingsData
-        if (needPassword && +lockScreenStatus) {
-            setLockScreenConfig({flowType : LockScreenFlowTypes.JUST_CALLBACK, callback : this._actualDelete })
-            NavStore.goNext('LockScreen')
-            return
-        }
+        const { walletHash, walletNumber } = this.props.selectedWalletData
 
-        if (!walletIsBackedUp && totalBalance * 1 > 0) {
-            setFlowType({ flowType: 'DELETE_WALLET', walletHash, walletNumber, source: 'AdvancedWalletScreen' })
-            NavStore.goNext('BackupStep1Screen')
-        } else {
-            setLoaderStatus(true)
-            try {
-                await walletActions.removeWallet(walletHash)
-                await UpdateAccountListDaemon.updateAccountListDaemon({force : true, source : 'AdvancedWalletScreen'})
-                setBseLink(null)
-                NavStore.goBack()
-                setLoaderStatus(false)
-            } catch (e) {
-                Log.log('WalletManagement.Advances deleteWallet error ' + e.message)
-                setLoaderStatus(false)
-            }
-        }
-
+        setFlowType({ flowType: 'DELETE_WALLET', walletHash, walletNumber, source: 'AdvancedWalletScreen' })
+        NavStore.goNext('BackupStep1Screen')
     }
 
     handleBack = () => { NavStore.goBack() }
@@ -120,6 +93,8 @@ class AdvancedWalletScreen extends PureComponent {
         MarketingAnalytics.setCurrentScreen('WalletManagement.Advances')
         const { colors, GRID_SIZE } = this.context
         const { isEditing, walletName } = this.state
+
+        const manyWallets = this.props.walletsLength > 1 ? true : false
 
         return (
             <ScreenWrapper
@@ -145,33 +120,10 @@ class AdvancedWalletScreen extends PureComponent {
                                     compRef={(ref) => { this.inputRef = ref }}
                                 />
                             ) : (
-                                <SwipeRow
-                                    leftOpenValue={50 + GRID_SIZE}
-                                    rightOpenValue={-(50 + GRID_SIZE)}
-                                    stopLeftSwipe={GRID_SIZE + 100}
-                                    stopRightSwipe={-(GRID_SIZE + 100)}
-                                    swipeToOpenPercent={30}
-                                    onRowPress={this.handleEdit}
-                                >
-                                    <View style={[styles.hiddenLayer, { paddingHorizontal: GRID_SIZE / 2 }]}>
-                                        <RoundButton
-                                            type="delete"
-                                            noTitle
-                                            onPress={this.handleDelete}
-                                            size={42}
-                                        />
-                                        <RoundButton
-                                            type="edit"
-                                            noTitle
-                                            onPress={this.handleEdit}
-                                            size={42}
-                                        />
-                                    </View>
-                                    <View style={[styles.visibleLayer, { backgroundColor: colors.walletManagment.advanceWalletNameBg }]}>
-                                        <Text style={[styles.walletNameLabel, { color: colors.common.text2 }]}>{strings('settings.walletManagement.advanced.walletNameLabel')} <CustomIcon name="edit" color={colors.common.text2} size={14} /></Text>
-                                        <Text style={[styles.walletNameValue, { color: colors.common.text1 }]}>{walletName}</Text>
-                                    </View>
-                                </SwipeRow>
+                                <TouchableOpacity onPress={this.handleEdit} style={[styles.visibleLayer, { backgroundColor: colors.walletManagment.advanceWalletNameBg }]}>
+                                    <Text style={[styles.walletNameLabel, { color: colors.common.text2 }]}>{strings('settings.walletManagement.advanced.walletNameLabel')} <CustomIcon name="edit" color={colors.common.text2} size={14} /></Text>
+                                    <Text style={[styles.walletNameValue, { color: colors.common.text1 }]}>{walletName}</Text>
+                                </TouchableOpacity>
                             )}
                         </View>
                         <ListItem
@@ -180,8 +132,17 @@ class AdvancedWalletScreen extends PureComponent {
                             iconType="key"
                             onPress={() => this.handleOpenRecoveryPhrase(true)}
                             rightContent="arrow"
-                            last
+                            last={!manyWallets}
                         />
+                        {manyWallets && 
+                            <ListItem
+                                title={strings('settings.walletManagement.advanced.deleteWallet')}
+                                iconType="delete"
+                                onPress={this.handleDelete}
+                                rightContent="arrow"
+                                last={manyWallets}
+                            />
+                        }
                     </View>
                 </ScrollView>
             </ScreenWrapper>
@@ -196,6 +157,7 @@ const mapStateToProps = (state) => {
         selectedWalletData: getSelectedWalletData(state),
         walletsGeneralData: getWalletsGeneralData(state),
         settingsData: getSettingsScreenData(state),
+        walletsLength: getWalletsNumber(state)
     }
 }
 
