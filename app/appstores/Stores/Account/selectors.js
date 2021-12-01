9a6b330b20e0c12cdf2e9fa80426f3f22255c0f5
ksu <ksu.zhytomirsky@gmail.com>
2021-12-01 09:45:19 +0200

    reformat

--- app/appstores/Stores/Account/selectors.js
+++ app/appstores/Stores/Account/selectors.js
@@ -1,55 +1,58 @@
-
 import { createSelectorCreator, defaultMemoize } from 'reselect'
 import _isEqual from 'lodash/isEqual'
 import _values from 'lodash/values'
 
 const DEFAULT_ACCOUNT = {
-  basicCurrencyRate: '', basicCurrencyBalance: '', basicCurrencySymbol: '', balancePretty: '', basicCurrencyBalanceNorm: ''
+    basicCurrencyRate: '',
+    basicCurrencyBalance: '',
+    basicCurrencySymbol: '',
+    balancePretty: '',
+    basicCurrencyBalanceNorm: ''
 }
 
 const createDeepEqualSelector = createSelectorCreator(
-  defaultMemoize,
-  _isEqual
+    defaultMemoize,
+    _isEqual
 )
 
 
 const selectAccountCurrency = (state, props) => {
-  try {
-    const currencyCode = props.cryptoCurrency.currencyCode
-    const selectedWallet = state.mainStore.selectedWallet.walletHash
+    try {
+        const currencyCode = props.cryptoCurrency.currencyCode
+        const selectedWallet = state.mainStore.selectedWallet.walletHash
 
-    if (typeof state.accountStore.accountList[selectedWallet] === 'undefined') {
-      // Log.log('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency') // @todo redo UpdateAccountListDaemon
-      return DEFAULT_ACCOUNT
-      // throw new Error('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency')
+        if (typeof state.accountStore.accountList[selectedWallet] === 'undefined') {
+            // Log.log('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency') // @todo redo UpdateAccountListDaemon
+            return DEFAULT_ACCOUNT
+            // throw new Error('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency')
+        }
+        if (typeof state.accountStore.accountList[selectedWallet][currencyCode] === 'undefined') {
+            // Log.log('Undefined currencyCode ' + selectedWallet + '  ' + currencyCode + ' in Account.selectors.selectAccountCurrency')
+            return DEFAULT_ACCOUNT
+            // throw new Error('Undefined currencyCode ' + selectedWallet + '  ' + currencyCode + ' in Account.selectors.selectAccountCurrency')
+        }
+        const account = state.accountStore.accountList[selectedWallet][currencyCode]
+        return account
+    } catch (e) {
+        throw new Error(e.message + ' in selectors.selectAccountCurrency')
     }
-    if (typeof state.accountStore.accountList[selectedWallet][currencyCode] === 'undefined') {
-      // Log.log('Undefined currencyCode ' + selectedWallet + '  ' + currencyCode + ' in Account.selectors.selectAccountCurrency')
-      return DEFAULT_ACCOUNT
-      // throw new Error('Undefined currencyCode ' + selectedWallet + '  ' + currencyCode + ' in Account.selectors.selectAccountCurrency')
-    }
-    const account = state.accountStore.accountList[selectedWallet][currencyCode]
-    return account
-  } catch (e) {
-    throw new Error(e.message + ' in selectors.selectAccountCurrency')
-  }
 }
 export const getAccountCurrency = createDeepEqualSelector(selectAccountCurrency, account => account)
 
 export function getAccountList(state) {
-  try {
-    const selectedWallet = state.mainStore.selectedWallet.walletHash
+    try {
+        const selectedWallet = state.mainStore.selectedWallet.walletHash
 
-    if (typeof state.accountStore.accountList[selectedWallet] === 'undefined') {
-      // Log.log('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency') // @todo redo UpdateAccountListDaemon
-      return DEFAULT_ACCOUNT
-      // throw new Error('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency')
+        if (typeof state.accountStore.accountList[selectedWallet] === 'undefined') {
+            // Log.log('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency') // @todo redo UpdateAccountListDaemon
+            return DEFAULT_ACCOUNT
+            // throw new Error('Undefined selectedWallet ' + selectedWallet + ' in Account.selectors.selectAccountCurrency')
+        }
+
+        const account = _values(state.accountStore.accountList[selectedWallet])
+        return account
+
+    } catch (e) {
+        throw new Error(e.message + ' in selectors.getAccountList')
     }
-
-    const account = _values(state.accountStore.accountList[selectedWallet])
-    return account
-
-  } catch(e) {
-    throw new Error(e.message + ' in selectors.getAccountList')
-  }
 }
