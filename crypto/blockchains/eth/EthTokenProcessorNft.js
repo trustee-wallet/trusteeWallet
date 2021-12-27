9f40c273af6d3153f850e5e427c73bd26ca2f9ee
ksu <ksu.zhytomirsky@gmail.com>
2021-12-27 16:07:29 +0200

    fx #2401

--- crypto/blockchains/eth/EthTokenProcessorNft.js
+++ crypto/blockchains/eth/EthTokenProcessorNft.js
@@ -6,6 +6,7 @@ import EthNftOpensea from '@crypto/blockchains/eth/apis/EthNftOpensea'
 import EthNftMatic from '@crypto/blockchains/eth/apis/EthNftMatic'
 import abi from './ext/erc721.js'
 import config from '@app/config/config'
+import BlocksoftDictNfts from '@crypto/common/BlocksoftDictNfts'
 
 export default class EthTokenProcessorNft extends EthBasic {
 
@@ -16,8 +17,9 @@ export default class EthTokenProcessorNft extends EthBasic {
      */
     async getListBlockchain(data) {
 
+        const settings = BlocksoftDictNfts.NftsIndexed[data.tokenBlockchainCode]
         if (
-            typeof this._settings.apiType !== 'undefined' && this._settings.apiType === 'OPENSEA'
+           typeof settings !== 'undefined' && typeof settings.apiType !== 'undefined' && settings.apiType === 'OPENSEA'
         ) {
             return EthNftOpensea(data)
         } else {
