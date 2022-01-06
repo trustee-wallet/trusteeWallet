b2b494d7522ff3a37281e1d6de6818a93df0ce5e
ksu <ksu.zhytomirsky@gmail.com>
2022-01-06 10:00:30 +0200

    cashback screen better (by V)

--- app/modules/Cashback/elements/QrCodePage.js
+++ app/modules/Cashback/elements/QrCodePage.js
@@ -15,20 +15,72 @@ import {
     Platform
 } from 'react-native'
 
-import { strings } from '@app/services/i18n'
 import QrCodeBox from '@app/components/elements/QrCodeBox'
 import CustomIcon from '@app/components/elements/CustomIcon'
+import TextInput from '@app/components/elements/new/TextInput'
+import Button from '@app/components/elements/new/buttons/Button'
+import qrLogo from '@assets/images/logoWithWhiteBG.png'
+
+import { strings } from '@app/services/i18n'
 import MarketingEvent from '@app/services/Marketing/MarketingEvent'
 import copyToClipboard from '@app/services/UI/CopyToClipboard/CopyToClipboard'
 import Toast from '@app/services/UI/Toast/Toast'
 import Log from '@app/services/Log/Log'
-import { ThemeContext } from '@app/theme/ThemeProvider'
+import ApiPromo from '@app/services/Api/ApiPromo'
+import config from '@app/config/config'
 
-import qrLogo from '@assets/images/logoWithWhiteBG.png'
+import { setLoaderStatus } from '@app/appstores/Stores/Main/MainStoreActions'
+import { showModal, hideModal } from '@app/appstores/Stores/Modal/ModalActions'
+
+import { ThemeContext } from '@app/theme/ThemeProvider'
 
 const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } = Dimensions.get('window')
 
 class QrCodePage extends PureComponent {
+
+    state = {
+        promoCode: ''
+    }
+
+    handleApplyPromoCode = async () => {
+        try {
+            setLoaderStatus(true)
+            let desc = await ApiPromo.activatePromo(this.state.promoCode)
+            if (typeof desc !== 'string') {
+                if (typeof desc['en'] !== 'undefined') {
+                    desc = desc['en']
+                } else {
+                    desc = JSON.stringify(desc)
+                }
+            }
+            showModal({
+                type: 'INFO_MODAL',
+                icon: false,
+                title: strings('modal.walletBackup.success'),
+                description: desc
+            })
+        } catch (e) {
+            if (config.debug.appErrors) {
+                console.log('CashBackScreen.Promo.handleApply error ' + e.message, e)
+            }
+            showModal({
+                type: 'INFO_MODAL',
+                icon: false,
+                title: strings('modal.exchange.sorry'),
+                description: strings('cashback.cashbackError.' + e.message)
+            })
+        }
+        setLoaderStatus(false)
+        this.handleDisablePromoCode()
+    }
+
+    handleDisablePromoCode = () => {
+        this.setState(() => ({ promoCode: '' }))
+    }
+
+    onChangeCode = (text) => {
+        this.setState({ promoCode: text })
+    }
 
     copyToClip = (token) => {
         MarketingEvent.logEvent('taki_cashback_3_copyToClip', { cashbackLink: token })
@@ -38,6 +90,61 @@ class QrCodePage extends PureComponent {
 
     handleRenderQrError = (e) => {
         if (e.message !== 'No input text') Log.err('CashbackScreen QRCode error ' + e.message)
+    }
+
+    handleBlur = () => {
+        if (this.state.promoCode === '') {
+            this.handleDisablePromoCode()
+            this.props.scrollToTop()
+        } else {
+            return null
+        }
+    }
+
+    renderModalContent = () => {
+
+        const {
+            colors,
+            GRID_SIZE
+        } = this.context
+
+        const buttonWidth = (WINDOW_WIDTH / 2) - GRID_SIZE * 1.5
+
+        return (
+            <View style={[styles.inputWrapper, { marginHorizontal: GRID_SIZE, marginBottom: GRID_SIZE }]}>
+                <TextInput
+                    onBlur={this.handleDisablePromoCode}
+                    numberOfLines={1}
+                    containerStyle={{ width: WINDOW_WIDTH - GRID_SIZE * 2 }}
+                    placeholder={strings('cashback.enterPromoPlaceholder')}
+                    onChangeText={this.onChangeCode}
+                    value={this.state.promoCode.value}
+                />
+                <View style={[styles.buttonsRow, { marginTop: GRID_SIZE }]}>
+                    <Button
+                        title={strings('assets.hideAsset')}
+                        onPress={hideModal}
+                        type='withoutShadow'
+                        containerStyle={{ backgroundColor: colors.common.button.bg, width: buttonWidth, marginRight: GRID_SIZE / 2 }}
+                    />
+                    <Button
+                        title={strings('walletBackup.settingsScreen.apply')}
+                        onPress={this.handleApplyPromoCode}
+                        type='withoutShadow'
+                        containerStyle={{ backgroundColor: colors.common.button.bg, width: buttonWidth, marginLeft: GRID_SIZE / 2 }}
+                    />
+                </View>
+            </View>
+        )
+    }
+
+    handleBackDropModal = () => {
+        showModal({
+            type: 'BACK_DROP_MODAL',
+            withMainButton: false,
+            disabledPresentationStyle: true,
+            Content: () => this.renderModalContent()
+        })
     }
 
     render() {
@@ -62,7 +169,7 @@ class QrCodePage extends PureComponent {
                     <Text style={[styles.pageSubtitle, { color: colors.common.text1, marginLeft: GRID_SIZE * 2.3, marginTop: GRID_SIZE }]}>{strings('cashback.pageSubtitle')}</Text>
 
                     <Image style={[styles.donut1, { marginTop: Platform.OS === 'ios' ? -GRID_SIZE * 4 : -GRID_SIZE * 2.5 }]} source={require('@assets/images/donut1.png')} />
-                    
+
                     <View style={styles.qrCodeContainer}>
                         <TouchableOpacity
                             style={[styles.qrBox, { padding: GRID_SIZE, backgroundColor: '#F5F5F5' }]}
@@ -81,7 +188,7 @@ class QrCodePage extends PureComponent {
                             />
                         </TouchableOpacity>
                         <TouchableOpacity
-                            style={[styles.tokenBox, { backgroundColor: colors.common.listItem.basic.iconBgLight, marginTop: GRID_SIZE, paddingHorizontal: GRID_SIZE * 1.5 }]}
+                            style={[styles.tokenBox, { backgroundColor: colors.common.listItem.basic.iconBgLight, marginTop: GRID_SIZE, paddingHorizontal: GRID_SIZE * 2 }]}
                             onPress={() => this.copyToClip(cashbackLink)}
                             activeOpacity={0.8}
                         >
@@ -91,6 +198,23 @@ class QrCodePage extends PureComponent {
                             </Text>
                         </TouchableOpacity>
                         <Text style={[styles.yourToken, { color: colors.common.text3, marginTop: GRID_SIZE / 2 }]}>{strings('cashback.yourToken')}</Text>
+
+                    </View>
+                    <View style={[styles.inviteContainer, styles.tokenBox, { marginTop: GRID_SIZE * 4, paddingHorizontal: GRID_SIZE * 1.6 }]}>
+                        <TouchableOpacity
+                            onPress={() => this.handleBackDropModal(this.state.promoCode)}
+                            activeOpacity={0.9}
+                        >
+                            <Text
+                                style={[
+                                    styles.showMnemonicButton,
+                                    {
+                                        marginHorizontal: GRID_SIZE,
+                                        color: colors.createWalletScreen.showMnemonic.showButtonText
+                                    }
+                                ]}
+                            >{strings('cashback.promoButton')}</Text>
+                        </TouchableOpacity>
                     </View>
                     <Image style={styles.donut2} source={require('@assets/images/donut2.png')} />
                     <Image style={styles.donuts} source={require('@assets/images/donuts.png')} />
@@ -123,13 +247,6 @@ const styles = StyleSheet.create({
         left: WINDOW_WIDTH * 0.772,
         width: WINDOW_WIDTH * 0.2125
     },
-    pageSubtitleProcent: {
-        fontFamily: 'Montserrat-SemiBold',
-        fontStyle: 'normal',
-        textAlign: 'center',
-        fontSize: WINDOW_WIDTH * 0.05,
-        color: '#ffffff'
-    },
     pageSubtitleText: {
         fontFamily: 'Montserrat-SemiBold',
         fontStyle: 'normal',
@@ -142,8 +259,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     qrBox: {
-        // flex: 1,
-
         borderRadius: 24,
         shadowColor: "#000",
         shadowOffset: {
@@ -153,24 +268,22 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.27,
         shadowRadius: 4.65,
 
-        elevation: 6,
+        elevation: 6
     },
     qrCode: {
-        alignSelf: 'center',
+        alignSelf: 'center'
     },
     qrCodeTokenString: {
         justifyContent: 'space-between',
         fontFamily: 'Montserrat-SemiBold',
         fontSize: WINDOW_WIDTH * 0.05,
         lineHeight: WINDOW_WIDTH * 0.06,
-        textAlign: 'center',
+        textAlign: 'center'
     },
     qrBg: {
         alignSelf: 'center',
-        // position: 'relative',
         width: WINDOW_WIDTH,
-        height: WINDOW_HEIGHT,
-        // marginTop: -100
+        height: WINDOW_HEIGHT
     },
     donut1: {
         position: 'relative',
@@ -182,17 +295,11 @@ const styles = StyleSheet.create({
     },
     buttonsRow: {
         flexDirection: 'row',
-        justifyContent: 'center'
-    },
-    buttonContainer: {
-        marginTop: 12,
-        flex: 1
+        justifyContent: 'space-between'
     },
     tokenBox: {
         borderRadius: 16,
-        // flex: 1,
-        height: 54,
-        width: '100%',
+        minHeight: 54,
         alignSelf: 'center',
         justifyContent: 'center',
         zIndex: 2
@@ -218,7 +325,23 @@ const styles = StyleSheet.create({
         width: WINDOW_WIDTH * 0.6,
         height: WINDOW_HEIGHT * 0.3,
         bottom: WINDOW_HEIGHT * (Platform.OS === 'ios' ? 0.16 : 0.11),
-        left: -WINDOW_WIDTH * 0.11,
+        left: -WINDOW_WIDTH * 0.2,
         resizeMode: 'contain'
+    },
+    inviteContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 20
+    },
+    showMnemonicButton: {
+        textAlign: 'center',
+        fontFamily: 'Montserrat-Bold',
+        fontSize: 12,
+        lineHeight: 15,
+        letterSpacing: 1.5,
+        textTransform: 'uppercase'
+    },
+    inputWrapper: {
+        alignItems: 'center'
     }
 })
