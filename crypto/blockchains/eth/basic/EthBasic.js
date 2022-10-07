fe78f9d49fc46abe8a311ecfd231b6f929a455ab
ksu <ksu.zhytomirsky@gmail.com>
2022-10-07 18:51:49 +0300

    ETH_POW

--- crypto/blockchains/eth/basic/EthBasic.js
+++ crypto/blockchains/eth/basic/EthBasic.js
@@ -103,7 +103,21 @@ export default class EthBasic {
             this._mainTokenType = 'ETC_ERC_20'
             this._mainTokenBlockchain = 'Ethereum Classic'
             this._mainChainId = 61 // https://ethereumclassic.org/development/porting
-        }  else if (settings.currencyCode === 'VLX' || (typeof settings.tokenBlockchain !== 'undefined' && settings.tokenBlockchain === 'VLX')) {
+        } else if (settings.currencyCode === 'ETH_POW' || (typeof settings.tokenBlockchain !== 'undefined' && settings.tokenBlockchain === 'ETH_POW')) {
+
+            this._etherscanSuffix = ''
+            this._etherscanApiPath = false
+            this._etherscanApiPathInternal = false
+            this._etherscanApiPathForFee = false
+
+            this._trezorServer = false
+            this._trezorServerCode = false
+
+            this._mainCurrencyCode = 'ETH_POW'
+            this._mainTokenType = 'ETH_POW_ERC_20'
+            this._mainTokenBlockchain = 'ETH_POW'
+            this._mainChainId = 10001
+        } else if (settings.currencyCode === 'VLX' || (typeof settings.tokenBlockchain !== 'undefined' && settings.tokenBlockchain === 'VLX')) {
 
             this._etherscanSuffix = ''
             this._etherscanApiPath = `https://evmexplorer.velas.com/api?module=account&sort=desc&action=txlist`
